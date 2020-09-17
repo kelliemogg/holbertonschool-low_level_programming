1c7ac11f419fa08d91aa9c09c4ff53cd9d6f2852
Kellie Mogg <kelliedmogg@gmail.com>
2020-09-17 03:08:27 +0000

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+/**
+* main- void
+* Description: print the size of various types on the computer
+* Return: 0
+*/
+#include <stdio.h>
+int main(void)
+{
+printf("Size of a char: %c\n");
+printf("Size of and int: %d\n");
+printf("Size of a long int: %ld\n");
+printf("Size of a long long int: %lld\n");
+printf("Size of a float: %f\n");
+return (0);
+}
