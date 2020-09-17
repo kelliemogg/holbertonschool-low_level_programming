cbcea510864f3b7c3e996f820872003e1643fd11
Kellie Mogg <kelliedmogg@gmail.com>
2020-09-17 02:12:36 +0000

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-
 /*
  * main- entry point
  * Return: 0 in success or other number in failure
  */
-
 int main(void)
 {
         puts("\"Programming is like building a multilingual puzzle");
