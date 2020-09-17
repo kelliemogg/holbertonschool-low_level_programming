372b6deef25eda67b7bf499f7178e69bace2e908
Kellie Mogg <kelliedmogg@gmail.com>
2020-09-17 03:47:09 +0000

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -18,10 +18,10 @@ c = 4;
 d = 8;
 e = 4;
 
-printf("Size of a char: %c\n", sizeof(char));
-printf("Size of an int: %d\n", sizeof(int));
-printf("Size of a long int: %ld\n", sizeof(long int));
-printf("Size of a long long int: %llu\n", sizeof(long long int));
-printf("Size of a float: %1.0f\n", sizeof(float));
+printf("Size of a char: %c\n", sizeof(char) a);
+printf("Size of an int: %d\n", sizeof(int) b);
+printf("Size of a long int: %ld\n", sizeof(long int) c);
+printf("Size of a long long int: %llu\n", sizeof(long long int) d);
+printf("Size of a float: %1.0f\n", sizeof(float) e);
 return (0);
 }
