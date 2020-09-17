a58920c71afa4be303b8ea5346abc9e0fcf3b46f
Kellie Mogg <kelliedmogg@gmail.com>
2020-09-17 15:46:30 +0000

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,16 +12,16 @@ long int c;
 long long int d;
 float e;
 
-a = 1 byte(s);
-b = 4 byte(s);
-c = 8 byte(s);
-d = 8 byte(s);
-e = 4 byte(s);
+a = 1;
+b = 4;
+c = 8;
+d = 8;
+e = 4;
 
-printf("Size of a char: %c\n", a);
-printf("Size of an int: %d\n", b);
-printf("Size of a long int: %ld\n", c);
-printf("Size of a long long int: %llu\n", d);
-printf("Size of a float: %1.0f\n", e);
+printf("Size of a char: %d byte(s)\n", a);
+printf("Size of an int: %d byte(s)\n", b);
+printf("Size of a long int: %ld byte(s)\n", c);
+printf("Size of a long long int: %llu byte(s)\n", d);
+printf("Size of a float: %1.0f byte(s)\n", e);
 return (0);
 }
