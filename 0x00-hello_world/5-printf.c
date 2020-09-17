8c3ae4edf8cd12d9f98d76a47b1154c4e5542cb2
Kellie Mogg <kelliedmogg@gmail.com>
2020-09-17 02:32:58 +0000

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+/**
+ * main- void
+ * Description: prints with proper grammar, but the outcome is a piece of art,
+ * Return: 0
+ */
+#include <stdio.h>
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
