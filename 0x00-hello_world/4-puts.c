b5f94fb1d57cde53fa7d7ff9262241c730929de7
Kellie Mogg <kelliedmogg@gmail.com>
2020-09-17 01:48:06 +0000

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
-int main(void)
+/*
+ *Print text as written
+*/
+
+int main (void)
 {
-    puts("Programming is like building a multilingual puzzle\n);
+    puts("\"Programming is like building a multilingual puzzle\n");
     return(0);
 }
