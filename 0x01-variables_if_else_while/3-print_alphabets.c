cc685e592b16436c931649fa896c1919e3b3ea20
Kellie Mogg <kelliedmogg@gmail.com>
2020-09-18 22:50:42 +0000

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,18 +4,16 @@
 *Description:
 *Return: 0
 */
+
 int main(void)
 {
-char n;
-char m;
+  char n, m;
 m = [[:upper:]]
 
 for (n = 'a'; n <= 'z'; n++)
 {
 putchar(n);
-}
 for (m = 'A'; m <= 'Z'; m++)
-{
 putchar(n);
 }
 putchar('\n');
