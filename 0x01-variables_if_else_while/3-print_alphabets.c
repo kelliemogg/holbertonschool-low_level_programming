32c2c83e4d99811fd54bef5fae35690da5f30101
Kellie Mogg <kelliedmogg@gmail.com>
2020-09-18 22:14:07 +0000

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+*Main- void
+*Description:
+*Return: 0
+*/
+int main(void)
+{
+char n;
+char m;
+m = [[:upper:]]
+
+for (n = 'a'; n <= 'z'; n++)
+{
+putchar(n);
+}
+for (m = 'A'; m <= 'Z'; m++)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
+}
