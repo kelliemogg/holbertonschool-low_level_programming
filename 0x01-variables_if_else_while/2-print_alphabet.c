0c64d3a1918f5229fe2c22e9721acef045510fcf
Kellie Mogg <kelliedmogg@gmail.com>
2020-09-18 19:12:07 +0000

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+*main- void
+*description: alphabet
+*Return: 0
+*/
+
+int main(void)
+{
+char n;
+
+for (n = 'a'; n <= 'z'; n++)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
+}
