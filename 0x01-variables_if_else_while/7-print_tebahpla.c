cb352efc9d8a1cd747db77bebc54c3b488f15fa0
Kellie Mogg <kelliedmogg@gmail.com>
2020-09-19 23:55:46 +0000

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+* main- void
+* description: dont tell me to smile
+* Return: 0
+**/
+
+int main(void)
+{
+char n;
+
+for (n = 'z'; n >= 'a'; n--)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
+}
