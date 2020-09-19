c9049eb6abdf1b3e1889a003df322567eebe6716
Kellie Mogg <kelliedmogg@gmail.com>
2020-09-19 04:53:42 +0000

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**                                                                                                          
+* main- void                                                                                                 
+* description: alphabet soup                                                                                 
+* Return: 0                                                                                                  
+**/
+
+#include <stdio.h>
+int main(void)
+return (0);
+}
