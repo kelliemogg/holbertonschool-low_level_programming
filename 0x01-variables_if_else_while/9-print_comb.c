cb26d3c60f3b0b1a113514e578236dca5a4feddc
Kellie Mogg <kelliedmogg@gmail.com>
2020-09-20 18:20:30 +0000

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+/**
+* main- void
+* description: number list
+* Return: 0
+*/
+#include <stdio.h>
+
+int main(void)
+{
+int x = '0';
+
+for (x = '0'; x <= '9'; x++)
+{
+putchar(x);
+if (x != '9')
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
