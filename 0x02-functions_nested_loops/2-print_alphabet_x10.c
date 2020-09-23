81f874bddbe16f6f391892a161f0c218798645f7
Kellie Mogg <kelliedmogg@gmail.com>
2020-09-23 23:25:18 +0000

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+* main- void
+* description: alphabet 10x
+*/
+
+void print_alphabet_x10(void)
+{
+int c;
+for (c = 'a'; c <= 'z'; c++)
+_putchar(c);
+_putchar(c);
+_putchar(c);
+_putchar(c);
+_putchar(c);
+_putchar(c);
+_putchar(c);
+_putchar(c);
+_putchar(c);
+_putchar(c);
+_putchar('\n');
+}
