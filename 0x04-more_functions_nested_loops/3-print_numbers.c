1b424c6d66c46e2e9bf71c24135aecb2b7d04a66
Kellie Mogg <kelliedmogg@gmail.com>
2020-09-24 21:54:10 +0000

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - prints numbers
+ * description: this prints numbers
+ */
+
+void print_numbers(void)
+{
+	int a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		_putchar('a');
+	}
+	_putchar('\n');
+}
