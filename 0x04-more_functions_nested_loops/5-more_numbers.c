0d5ce69bea3337ebca4ee8ce7a55d470cb861081
Kellie Mogg <kelliedmogg@gmail.com>
2020-09-24 23:08:15 +0000

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - numbers on numbers
+ * description: row of numbers
+ */
+
+void more_numbers(void)
+{
+	int a, r;
+
+	for (r = 0; r <= 9; r++)
+	{
+		for (a = 0; a <= 14; a++)
+		{
+			_putchar((a / 10) + '0');
+			_putchar((a % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
