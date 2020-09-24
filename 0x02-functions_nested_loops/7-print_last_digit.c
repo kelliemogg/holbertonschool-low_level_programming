1203b6003bda238fe8ad7cb79766330c46077426
Kellie Mogg <kelliedmogg@gmail.com>
2020-09-24 02:48:22 +0000

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+* print_last_digit - integer
+* description: print last digit
+* @n: variable
+* Return: last digit
+*/
+
+int print_last_digit(int n)
+{
+if (n < 0)
+{
+n = (n * -1);
+}
+_putchar (n % 10 + '0');
+return (n % 10);
+}
