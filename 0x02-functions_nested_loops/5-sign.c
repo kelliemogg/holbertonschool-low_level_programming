e996296d87e1b5b4c25aca16f0e22b352e69a24c
Kellie Mogg <kelliedmogg@gmail.com>
2020-09-24 02:07:04 +0000

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+* print_sign - print signs
+* @n: this is the variable
+* description: here is were we print signs
+* Return: 1, 0, -1
+*/
+
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+else if (n < 0)
+{
+_putchar('-');
+return (-1);
+}
+}
