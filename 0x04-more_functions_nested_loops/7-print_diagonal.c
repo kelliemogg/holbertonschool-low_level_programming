d1956a3e0ec99bc617b720f524997eea999d78d9
Kellie Mogg <kelliedmogg@gmail.com>
2020-09-25 01:36:53 +0000

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - slanty line
+ * description: slanty line
+ * @n: variables
+ */
+
+void print_diagonal(int n)
+{
+	int r;
+	int s;
+
+	for (r = 0; r < n; r++)
+	{
+		for (s = 0; s < r; s++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	if (n < 0)
+	_putchar('\n');
+	}
+}
