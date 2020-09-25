edc6452b4f95b13c816419db9d9dc26f2d154300
Kellie Mogg <kelliedmogg@gmail.com>
2020-09-25 02:24:29 +0000

    101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include "holberton.h"
+
+/**
+ * print_number - print numba
+ * @n: this is a variable
+ * description: idk what this does yet
+ * Return: 0
+ */
+
+void print_number(int n);
+
+{
+	return (0);
+}
