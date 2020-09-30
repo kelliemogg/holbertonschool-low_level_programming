233a11d1f151ed5df9812999c3b318970de10200
Kellie Mogg <kelliedmogg@gmail.com>
2020-09-30 00:14:50 +0000

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * _strlen - check the code for Holberton School students.
+ * description: write a function
+ * @s: string
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int x;
+
+	for (x = 0; s[x] != '\0'; ++x)
+	return (x);
+}
