31763a510b1640aeb05835f3fad53808889daadc
Kellie Mogg <kelliedmogg@gmail.com>
2020-09-30 04:34:37 +0000

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * _strcpy - and away we go
+ * description: pointers
+ * @dest: variable
+ * @src: variable
+ * Return: pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int x;
+
+	return (0);
+}
