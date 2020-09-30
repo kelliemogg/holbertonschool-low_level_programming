fe2b91b0e89e9af1d0dcea4de0b1c299039dead4
Kellie Mogg <kelliedmogg@gmail.com>
2020-09-30 04:34:18 +0000

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * print_array - print an array
+ * description: lol at the presidential debate
+ * @a: variable
+ * @n: variable
+ * Return: 0
+ */
+
+void print_array(int *a, int n)
+{
+	int x;
+
+	return (0);
+}
