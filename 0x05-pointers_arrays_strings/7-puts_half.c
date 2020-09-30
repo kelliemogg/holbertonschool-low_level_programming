c27d5504c85a29653361822c2d532f9a453c1898
Kellie Mogg <kelliedmogg@gmail.com>
2020-09-30 04:33:56 +0000

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * puts2 - put it down
+ * description: lol at the presidential debate
+ * @str: stringgg
+ * Return: 0
+ */
+
+void rev_string(char *s)
+{
+	int x;
+
+	return (0);
+}
