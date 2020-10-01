75345bd3a0032284eddfc8ced47906beed82ba04
Kellie Mogg <kelliedmogg@gmail.com>
2020-10-01 22:12:20 +0000

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,6 +1,24 @@
 #include "holberton.h"
 
 /**
+ * _strlen - string length
+ * description: cat two strings
+ * @s: variable
+ * Return: pointer to dest
+ */
+
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (s[count] != '\0')
+	{
+		count++;
+	}
+	return (count);
+}
+
+/**
  * _strncat - and away we go
  * description: cat two strings
  * @dest: variable
@@ -11,13 +29,12 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, i2;
+	int i;
+	int srclen = _strlen(src);
+	int destlen = _strlen(dest);
 
-	for (i = 0; i < n && dest[i] != '\0'; i++)
-	{}
-	for (i2 = 0; i2 < n && src[i2] != '\0'; i2++)
-	{}
-	i = 0;
-	*dest = dest[i] + src[i2];
+	for (i = 0; i <= srclen && i < n; i++)
+		dest[destlen + i] = src[i];
+
 	return (dest);
 }
