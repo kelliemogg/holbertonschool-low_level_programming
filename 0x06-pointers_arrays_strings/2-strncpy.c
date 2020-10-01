4cbdc9facdf40705de411441a1c32c4635b2dd98
Kellie Mogg <kelliedmogg@gmail.com>
2020-10-01 19:59:17 +0000

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -6,12 +6,13 @@
  * @dest: variable
  * @src: variable
  * @n: variable
- * Return:
+ * Return: dest
  */
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int x;
-
-	return (0);
+	for (n = 0; src[n] != 0; n++)
+	{}
+	*dest = src[n];
+	return (dest);
 }
