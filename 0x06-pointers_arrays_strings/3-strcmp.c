6dbd465a5211cdecf7a7d9f558fa4db945effda4
Kellie Mogg <kelliedmogg@gmail.com>
2020-10-01 23:22:11 +0000

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,12 +5,17 @@
  * description: strcmp
  * @s1: variable
  * @s2: variable
- * Return:
+ * Return: int
  */
 
-char *_strcmp(char *s1, char *s2)
+int _strcmp(char *s1, char *s2)
 {
-	int x;
+	int i = 0;
 
-	return (0);
+	if (s2[i] > s1[i])
+		return (s1[i] - s2[i]);
+	else if (s1[i] > s2[i])
+		return (s1[i] - s2[i]);
+	else
+		return (0);
 }
