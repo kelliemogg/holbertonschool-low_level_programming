b54b7525924a1c41f36ca601e4b7fa2833da999d
Kellie Mogg <kelliedmogg@gmail.com>
2020-10-01 22:20:41 +0000

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,6 +1,24 @@
 #include "holberton.h"
 
 /**
+ * _strlen - string length
+ * description: string length
+ * @s: variable
+ * Return: count
+ */
+
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (s[count] != '\0')
+	{
+		count++;
+	}
+	return (count);
+}
+
+/**
  * _strncpy - and away we go
  * description: pointers
  * @dest: variable
@@ -11,8 +29,12 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	for (n = 0; src[n] != 0; n++)
-	{}
-	*dest = src[n];
+	int i;
+	int srclen = _strlen(src);
+
+	for (i = 0; i <= srclen && i < n; i++)
+		dest[i] = src[i];
+	for (; i < n; i++)
+		dest[i] = '\0';
 	return (dest);
 }
