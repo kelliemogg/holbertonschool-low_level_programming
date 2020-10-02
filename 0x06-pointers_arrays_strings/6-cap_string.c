f2eb6cc97d119b489ed60de370dacd74633e5387
Kellie Mogg <kelliedmogg@gmail.com>
2020-10-02 19:12:19 +0000

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,13 +3,45 @@
 /**
  * cap_string - and away we go
  * description: capitalize
- * @cap_string: variable
- * Return:
+ * @str: variable
+ * Return: str
  */
 
-char *cap_string(char *)
+char *cap_string(char *str)
 {
-	int x;
+	int i = 0;
+	int j = 0;
 
-	return (0);
+	char arr[13];
+
+	arr[0] = ',';
+	arr[1] = ';';
+	arr[2] = '.';
+	arr[3] = '!';
+	arr[4] = '?';
+	arr[5] = '"';
+	arr[6] = '(';
+	arr[7] = ')';
+	arr[8] = '{';
+	arr[9] = '}';
+	arr[10] = ' ';
+	arr[11] = '\t';
+	arr[12] = '\n';
+
+	str[i] = (i == 0 && str[i] <= 'z' && str[i] >= 'a') ? str[i] - 32 : str[i];
+
+	while (str[i])
+	{
+		while (j < 13)
+		{
+			if (str[i] == arr[j] && str[i + 1] >= 'a' && str[i + 1] <= 'z')
+			{
+				str[i + 1] -= 32;
+			}
+				j++;
+		}
+		i++;
+		j = 0;
+	}
+	return (str);
 }
