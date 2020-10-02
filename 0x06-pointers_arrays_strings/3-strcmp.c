d4c848cdce2297881aa113bd46eebe922d6623ce
Kellie Mogg <kelliedmogg@gmail.com>
2020-10-02 01:45:29 +0000

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -15,8 +15,8 @@ int _strcmp(char *s1, char *s2)
 	while ((s1[i] <= '\0' && s2[i] <= '\0') && s1 == s2)
 	i++;
 
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
-		else
+		if (s1[i] == s2[i])
 			return (0);
+		else
+			return (s1[i] - s2[i]);
 }
