832c36966374088401caa994508917e585667de8
Kellie Mogg <kelliedmogg@gmail.com>
2020-10-05 18:31:26 +0000

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,1 +1,20 @@
 #include "holberton.h"
+
+/**
+ * _memset - filling memory
+ * description: filling memory
+ * @s: memory area
+ * @b: constant byte
+ * @n: amount of memory to be filled
+ * @i: unsigned
+ * Return: s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; (unsigned)s[i] <= n; i++)
+		s[i] = b;
+	return (s);
+}
