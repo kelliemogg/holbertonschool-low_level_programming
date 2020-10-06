7babc0040feb4c54538935c57bfe106b0c66ce90
Kellie Mogg <kelliedmogg@gmail.com>
2020-10-06 01:58:46 +0000

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,16 +1,37 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
- * _strcpy - and away we go
- * description: pointers
- * @dest: variable
- * @src: variable
- * Return: pointer to dest
+ * _strlen - counts the number of characters in a string
+ * @s: This is a pointer to a string
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (s[count] != '\0')
+	{
+		count++;
+	}
+	return (count);
+}
+
+/**
+ * _strcpy - copies a string to another pointer
+ * @dest: copying to this pointer
+ * @src: input string
+ * Return: void
  */
 
 char *_strcpy(char *dest, char *src)
 {
-	int x;
+	int i;
+	int count = _strlen(src);
 
-	return (0);
+	for (i = 0; i <= count; i++)
+		dest[i] = src[i];
+
+	return (dest);
 }
