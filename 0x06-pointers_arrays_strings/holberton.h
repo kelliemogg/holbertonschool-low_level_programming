90095dad3eb62e2216397ec9bacd78d00eb04dcd
Kellie Mogg <kelliedmogg@gmail.com>
2020-10-06 18:51:48 +0000

    holberton.h

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -1,6 +1,7 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
+char *_memcpy(char *dest, char *src, unsigned int n);
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -9,7 +10,7 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
-char *leet(char *);
+char *leet(char *str);
 char *rot13(char *);
 
 #endif
