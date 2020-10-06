5688e34bc6aa43164f5e41a7ddbcd2707372c27f
Kellie Mogg <kelliedmogg@gmail.com>
2020-10-06 01:24:55 +0000

    8-print_diagsums.c

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * print_diagsums - two diagonals
+ * description: two diagonals
+ * @a: variable
+ * @size: variable
+ * Return: 0
+ */
+
+void print_diagsums(int *a, int size)
+{
+	return(0);
+}
