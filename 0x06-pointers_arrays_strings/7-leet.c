e49a4e6d4137bd9059f5adf37279d2af8fad9c27
Kellie Mogg <kelliedmogg@gmail.com>
2020-10-06 02:01:52 +0000

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,15 +1,36 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
- * leet - and away we go
- * description: pointers
- * @leet: variable
- * Return: pointer to dest
+ * _strlen - counts the number of characters in a string
+ * @s: This is a pointer to a string
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (s[count] != '\0')
+	{
+		count++;
+	}
+	return (count);
+}
+
+/**
+ * leet - encode characters to 1337
+ * @*: a string
+ * Return: encoded string
  */
 
 char *leet(char *)
 {
-	int x;
+	int i;
+	int count = _strlen(src);
 
-	return (0);
+	for (i = 0; i <= count; i++)
+		dest[i] = src[i];
+
+	return (dest);
 }
