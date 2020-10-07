5ccd0de01f55193bbfa465590029d9c49cff0a14
Kellie Mogg <kelliedmogg@gmail.com>
2020-10-07 18:41:34 +0000

    holberton.h

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -1,6 +1,7 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 
+int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -10,4 +11,4 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 
-#endif
+#endif /* HOLBERTON_H */
