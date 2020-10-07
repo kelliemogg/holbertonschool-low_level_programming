c3537d37eb1e4fe1ed5f0de2c6bbc2433c3141a4
Kellie Mogg <kelliedmogg@gmail.com>
2020-10-07 23:16:56 +0000

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -4,10 +4,32 @@
  * _sqrt_recursion - returns a value
  * description: returns the natural sq root of a number
  * @n: number
- * Return: new value for n
+ * Return: square
  */
 
 int _sqrt_recursion(int n)
 {
-	return(new);
+	int x = 0;
+
+	return (square(n, x));
+}
+
+/**
+ * square - square func
+ * description: natural square root
+ * @x: variable
+ * @n: constant
+ * Return: x, -1, square
+ */
+
+int square(int n, int x)
+{
+        if ((x * x) == n)
+                return (x);
+
+        else if ((x * x) > n)
+                return (-1);
+
+        else
+                return (square(n, x + 1));
 }
