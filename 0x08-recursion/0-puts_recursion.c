1a65b8ac6d7ad03cc88b3765574c2470c231fc52
Kellie Mogg <kelliedmogg@gmail.com>
2020-10-07 16:05:41 +0000

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,5 +9,14 @@
 
 void _puts_recursion(char *s)
 {
-	return(0);
+	int i = 0;
+
+	if (*(s + i) != '\0')
+	{
+		_putchar(*(s + i));
+		i++;
+		_puts_recursion(s + i);
+	}
+	else
+		_putchar('\n');
 }
