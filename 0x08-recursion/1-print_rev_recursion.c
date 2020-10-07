600c03f9a63e04d3babf3acdd9f33db5d1a02e59
Kellie Mogg <kelliedmogg@gmail.com>
2020-10-07 19:22:58 +0000

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -4,10 +4,17 @@
  * _print_rev_recursion - reverse string
  * description: prints a string in reverse
  * @s: string
- * Return: 0
+ * Return: void
  */
 
 void _print_rev_recursion(char *s)
 {
-	return(0);
+	int i = 0;
+
+	if (*(s + i) != '\0')
+	{
+		i++;
+		_print_rev_recursion(s + i);
+		_putchar(*(s + i));
+	}
 }
