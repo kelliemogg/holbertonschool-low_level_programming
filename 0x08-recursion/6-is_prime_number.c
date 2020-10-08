57a739488d2fc3ccd202d790ee5e055516bddcc5
Kellie Mogg <kelliedmogg@gmail.com>
2020-10-08 00:49:59 +0000

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,5 +9,31 @@
 
 int is_prime_number(int n)
 {
-	return(name);
+	int x = n - 1;
+
+	if (n < 2)
+	{
+		return(0);
+	}
+	return (prime(n, x));
+}
+
+/**
+ * prime - my function
+ * description: prime number test
+ * @n: number to pass
+ * @x: variable
+ * Return: 1, 0
+ */
+
+int prime(int n, int x)
+{
+	if (x == 1)
+		return (1);
+	{
+	if (n % x == 0)
+		return (0);
+	else
+		return (prime(x - 1, n));
+	}
 }
