a6c91041ac6b18c9e80a3fefd0761f01d898df44
Kellie Mogg <kelliedmogg@gmail.com>
2020-10-08 01:18:57 +0000

    7-is_palindrome.c

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -1,13 +1,49 @@
 #include "holberton.h"
 
 /**
- * is_palindrome - function
+ * _strlen_recursion - function
  * description: palindrome or not
+ * @s: string
+ * Return: 1, 0
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		return (1 + (_strlen_recursion(s + 1)));
+	}
+	return (0);
+}
+
+/**
+ * _print_rev_recursion - print it
+ * description: print in reverse
+ * @s: string
+ * Return: void
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	_print_rev_recursion(s + 1);
+}
+
+/**
+ * is_palindrome - palindrome?
+ * description: is it a palindrome?
  * @s: string
  * Return: 1, 0
  */
 
 int is_palindrome(char *s)
 {
-	return(1, 0);
+	if (*s == *s)
+	{
+		return (1);
+	}
+	return (0);
 }
