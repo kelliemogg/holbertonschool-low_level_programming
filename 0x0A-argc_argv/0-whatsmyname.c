07cb498c46d7556f0917654f7df88c6320de05e3
Kellie Mogg <kelliedmogg@gmail.com>
2020-10-11 18:43:48 +0000

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * main - program
+ * description: mynameis
+ * @argc: argc
+ * @argv: argv
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc >= 0)
+		printf("%s\n", argv[0]);
+	return (0);
+}
