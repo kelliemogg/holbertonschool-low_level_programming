63ad713202e8fa8d1f2fc81320f3d4ec4ef083c8
Kellie Mogg <kelliedmogg@gmail.com>
2020-10-11 23:03:58 +0000

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -18,11 +18,11 @@ int main(int argc, char *argv[])
 	{
 		mul = atoi(argv[i + 1]) * atoi(argv[i]);
 		printf("%d\n", mul);
+		return (0);
 	}
 	else
 	{
 		printf("Error \n");
 		return (1);
 	}
-	return (0);
 }
