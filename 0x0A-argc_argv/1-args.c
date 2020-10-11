e64e47ace5d20ef48abd3b20a78e314d6dadd0d4
Kellie Mogg <kelliedmogg@gmail.com>
2020-10-11 19:16:58 +0000

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * main - number of arguments
+ * description: passed into the program
+ * @argc: argc
+ * Return: 0
+ */
+
+int main(int argc, char* argv[])
+{
+	(void)argv;
+	printf("argc = %d\n", argc - 1);
+	return (0);
+}
