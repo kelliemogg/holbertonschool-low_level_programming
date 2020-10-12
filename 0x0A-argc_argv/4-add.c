1bd18722157a2830d75be8dbd39a49e8743228dd
Kellie Mogg <kelliedmogg@gmail.com>
2020-10-12 02:38:25 +0000

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * main - main
+ * description: adds positive numbers
+ * @argc: argc
+ * @argv: argv
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+	int sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		if (atoi(argv[i]) == 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
