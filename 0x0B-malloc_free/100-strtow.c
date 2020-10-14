72a6f39858ebdc667bf97306cc730b470735b1cf
Kellie Mogg <kelliedmogg@gmail.com>
2020-10-14 20:10:14 +0000

    100-strtow.c

---
+++ 0x0B-malloc_free/100-strtow.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * strtow - strtow
+ * description: strtow
+ * @str: string
+ * Return: NULL
+ */
+
+char **strtow(char *str)
+{
+	if (str == NULL)
+		return (NULL);
+
+	if (str == "")
+		return (NULL);
+}
