99c965d3e18600be10af6583332b6760a5dcf2c0
Kellie Mogg <kelliedmogg@gmail.com>
2020-10-14 20:19:32 +0000

    100-strtow.c

--- 0x0B-malloc_free/100-strtow.c
+++ 0x0B-malloc_free/100-strtow.c
@@ -11,9 +11,22 @@
 
 char **strtow(char *str)
 {
+	int i;
+	int buffer = 0, length = 0;
+	char **ptr;
+
 	if (str == NULL)
 		return (NULL);
 
 	if (str == "")
 		return (NULL);
+
+	for (i = 0; ptr[i]; i++)
+		length--;
+	ptr = (char *)malloc(length * sizeof(char));
+	if (!ptr)
+		return (NULL);
+	for (i = 0; str[i]; i++)
+		return (ptr);
+
 }
