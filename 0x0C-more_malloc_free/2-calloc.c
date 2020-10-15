3cc20429fca0b5e9d8a4192adcbfe5152587ba30
Kellie Mogg <kelliedmogg@gmail.com>
2020-10-15 21:16:50 +0000

    2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * _calloc - calloc
+ * description: calloc
+ * @nmemb: nmemb
+ * @size: size
+ * Return: NULL
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int x;
+	unsigned int *ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc(nmemb * size);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (x = 0; x < nmemb; x++)
+	{}
+	return (ptr);
+}
