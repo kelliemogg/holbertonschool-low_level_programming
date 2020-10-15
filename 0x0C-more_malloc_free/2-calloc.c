0b32dd16b6585acbdc6a86ab6022c0478e77413d
Kellie Mogg <kelliedmogg@gmail.com>
2020-10-15 21:31:18 +0000

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -22,7 +22,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (ptr == NULL)
 		return (NULL);
 
-	for (x = 0; x < nmemb; x++)
+	for (x = 0; x < nmemb * size; x++)
 	{
 		ptr[x] = 0;
 	}
