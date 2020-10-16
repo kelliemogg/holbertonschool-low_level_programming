f2ec488cc49ffabbb9f7463cbea6cb729008d89d
Kellie Mogg <kelliedmogg@gmail.com>
2020-10-16 20:30:02 +0000

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -3,6 +3,27 @@
 #include <stdlib.h>
 
 /**
+ * _memcpy - copy
+ * description: copy dat
+ * @n: n
+ * @dest: dest
+ * @src: src
+ * Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
+
+/**
  * _realloc - reallocate
  * @ptr: ptr
  * @old_size: old size
@@ -12,6 +33,43 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
+	char *new;
+
 	if (new_size == old_size)
 		return (ptr);
+
+	if (ptr == NULL)
+	{
+		ptr = malloc(new_size);
+		return (ptr);
+	}
+
+	if (new_size > old_size)
+	{
+		new = malloc(new_size);
+
+		if (!new)
+			return (NULL);
+
+		_memcpy(new, ptr, old_size);
+
+		free(ptr);
+		return (new);
+	}
+
+	if (new_size == 0 || ptr != NULL)
+	{
+		free(ptr);
+			return (NULL);
+	}
+
+	new = malloc(new_size);
+
+		if (!new)
+			return (NULL);
+
+		_memcpy(new, ptr, old_size);
+
+		free(ptr);
+		return (new);
 }
