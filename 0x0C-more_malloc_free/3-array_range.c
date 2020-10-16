a3f39cfdcf4ec604dd314214e11bc47407d29001
Kellie Mogg <kelliedmogg@gmail.com>
2020-10-16 00:38:45 +0000

    3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,23 +1,27 @@
 #include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 /**
  * array_range - array range
  * @min: min
  * @max: max
- * Return: Null
+ * Return: arr
  */
 
 int *array_range(int min, int max)
 {
-	char *arr;
+	int *arr;
+	int i;
+	int mm = max - min + 1;
 
 	if (min > max)
 		return (NULL);
 
-	arr = malloc((min + max) * sizeof(char));
+	arr = malloc(sizeof(char) * mm);
+
+	for (i = 0; i < mm; i++)
+		arr[i] = min++;
 
 	return (arr);
 }
