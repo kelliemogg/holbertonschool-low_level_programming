abdef97d201e043a65f65108e4a24be83067a016
Kellie Mogg <kelliedmogg@gmail.com>
2020-10-19 21:35:18 +0000

    1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * init_dog - my dog
+ * description: name, age, owner
+ * @d: my dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: owner
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
