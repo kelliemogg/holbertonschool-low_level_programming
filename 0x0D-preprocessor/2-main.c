31124c98cff27598c4987276894315831e96b017
Kellie Mogg <kelliedmogg@gmail.com>
2020-10-19 19:22:27 +0000

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * main - main
+ * description: printing a program
+ * argc: argc
+ * argv: argv
+ * Return: 0
+ */
+
+int main()
+{
+	printf("%s\n", __BASE_FILE__);
+	return (0);
+}
