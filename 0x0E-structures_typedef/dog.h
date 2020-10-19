634de98c5e27f3fe12b8387918862542ebad2d38
Kellie Mogg <kelliedmogg@gmail.com>
2020-10-19 21:08:15 +0000

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 
+/**
+ * struct dog - struct of dog
+ * description: name, age, owner of dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+
 struct dog
 {
 	char *name;
