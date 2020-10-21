f3c00ca420ccfe6f473f456b3ffe7152b8e69027
Kellie Mogg <kelliedmogg@gmail.com>
2020-10-21 19:08:20 +0000

    1-array_iterator.c

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * array_iterator - ay i
+ * description: exe a function
+ * @array: pointer array
+ * @size: type size_t
+ * @action: pointer
+ * Return: void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (*array != NULL)
+		(*action)(*array[size]);
+}
