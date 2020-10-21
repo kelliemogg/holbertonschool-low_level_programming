f9218207afc239d0c6a822f89fd04875ca47f9c1
Kellie Mogg <kelliedmogg@gmail.com>
2020-10-21 18:40:51 +0000

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -6,12 +6,11 @@
  * print_name - void function
  * description: prints a name
  * @name: name
- * @char: char
  * @f: pointer function
  * Return: void
  */
 
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	(*f)(name);
 }
