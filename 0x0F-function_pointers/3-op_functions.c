77c55ae677e366ffa51e9d7d3b75940a558c8188
Kellie Mogg <kelliedmogg@gmail.com>
2020-10-22 02:00:50 +0000

    3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,24 +1,68 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * op_add - add
+ * description: add
+ * @a: a
+ * @b: b
+ * Return: 0
+ */
+
 int op_add(int a, int b)
 {
-	printf("%d\n", a + b);
+	return(a + b);
 }
+
+/**
+ * op_sub - sub
+ * description: sub
+ * @a: a
+ * @b: b
+ * Return: 0
+ */
 
 int op_sub(int a, int b)
 {
-	printf("%d\n", a - b);
+	return(a - b);
 }
+
+/**
+ * op_mul - mul
+ * description: mul
+ * @a: a
+ * @b: b
+ * Return: 0
+ */
 
 int op_mul(int a, int b)
 {
-	printf("%d\n", a * b);
+	return(a * b);
 }
+
+/**
+ * op_div - div
+ * description: div
+ * @a: a
+ * @b: b
+ * Return: 0
+ */
 
 int op_div(int a, int b)
 {
-	printf("%d\n", a / b);
+	return(a / b);
 }
+
+/**
+ * op_mod - mod
+ * description: mod
+ * @a: a
+ * @b: b
+ * Return: 0
+ */
 
 int op_mod(int a, int b)
 {
-	printf("%d\n", a % b);
+	return(a % b);
 }
