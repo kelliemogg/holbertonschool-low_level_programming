f59893d73fa2c09864b18caf238eeb1a67a4b82d
Kellie Mogg <kelliedmogg@gmail.com>
2020-10-22 02:57:44 +0000

    100-main_opcodes.c

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * number_of_bytes - numb of bytes
+ * description: adv
+ * Return: 0
+ */
+
+main number_of_bytes(int)
+{
+	int x = 0;
+
+	printf("0x%x\n", x);
+	printf("Error\n");
+}
