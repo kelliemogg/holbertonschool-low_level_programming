dde9aab0cf38e7a4c927c3784b0a655c8268c1e2
Kellie Mogg <kelliedmogg@gmail.com>
2020-10-26 03:56:20 +0000

    variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -21,5 +21,6 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+ssize_t write(int fd, const void *buf, size_t count);
 
 #endif /* _VARIADIC_FUNCTIONS_H_ */
