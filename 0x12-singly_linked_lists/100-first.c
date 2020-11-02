3e1d58b7c3b6122495e1cb05374ee09a390a9cf0
Kellie Mogg <kelliedmogg@gmail.com>
2020-11-02 20:32:19 +0000

    100

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -9,6 +9,6 @@ void print_b4_main(void) __attribute__((constructor));
 
 void print_b4_main(void)
 {
-	printf("You're beat! and yet, you must allow.\n");
+	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
