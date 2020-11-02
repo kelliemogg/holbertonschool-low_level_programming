a4191b146def9eb73f96c0ceff304ace703e9575
Kellie Mogg <kelliedmogg@gmail.com>
2020-11-02 16:21:51 +0000

    lol brackets

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,15 +13,13 @@ size_t print_list(const list_t *h)
 {
 	size_t x = 0;
 
-	while (h)
+	while (h != NULL)
 	{
-		if (h->str != NULL)
-		{
-		printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
-		}
-		else if (h->str == NULL)
+		if (h->str == NULL)
 			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
 		x++;
 	}
 	return (x);
