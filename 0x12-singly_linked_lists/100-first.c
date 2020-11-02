6b3f69959d01132851fd79698acf92dd17781378
Kellie Mogg <kelliedmogg@gmail.com>
2020-11-02 20:02:31 +0000

    oh

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+void print_b4_main(void) __attribute__((constructor));
+
+/**
+ * print_b4_main - print b4 main
+ * description: this then that
+ * Return: 0
+ */
+
+void print_b4_main(void)
+{
+	printf("You're beat! and yet, you must allow.\n");
+	printf("I bore my house upon my back!\n");
+}
