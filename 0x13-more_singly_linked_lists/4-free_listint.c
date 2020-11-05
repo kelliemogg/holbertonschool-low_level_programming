acb05b21d491dd7ad007dafed1d651d0d7e590cd
Kellie Mogg <kelliedmogg@gmail.com>
2020-11-05 03:27:30 +0000

    4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * free_listint - free
+ * description: free node
+ * @head: head
+ * Return: void
+ */
+
+void free_listint(listint_t *head)
+{
+	free(listint_t);
+	return;
+}
