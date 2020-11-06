32bfa234d1e795d5bad003a0f6460861a6245b6e
Kellie Mogg <kelliedmogg@gmail.com>
2020-11-06 02:23:01 +0000

    ugh

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * print_binary - print
+ * description: print the binary rep
+ * @n: numb
+ * Return: void
+ */
+
+void print_binary(unsigned long int n)
+{
+	int i = 0;
+	int x = 0;
+
+	for (i = 1 << 16; i > 0; i = i /2)
+	{
+		x = (n & i);
+		_putchar(x);
+	}
+}
