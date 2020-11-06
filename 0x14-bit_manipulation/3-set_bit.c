223fc315331204a3023c043d444a589c03ab0db8
Kellie Mogg <kelliedmogg@gmail.com>
2020-11-06 04:02:52 +0000

    three

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * set_bit - set bit
+ * description: set bit
+ * @n: n
+ * @index: index
+ * Return: 1, -1
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	for (index = 0; index < '\0'; index++)
+		;
+	return (1)
+}
