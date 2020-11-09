1fa68ba925d22b99e319508eebd0916048605b24
Kellie Mogg <kelliedmogg@gmail.com>
2020-11-09 23:19:45 +0000

    error checks

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -18,14 +18,21 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	ptr = malloc(sizeof(char) * letters);
+	if (ptr == NULL)
+		return (0);
 
 	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
 
 	count = read(fd, ptr, letters);
+	if (count == -1)
+		return (0);
 
 	value = write(STDOUT_FILENO, ptr, count);
-
+	if (value == -1)
+		return (0);
+	free(ptr);
 	close(fd);
-
 	return (value);
 }
