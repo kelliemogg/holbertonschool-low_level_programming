c6bf3cf5cc458de978bf3311aeabe310dc041d1f
Kellie Mogg <kelliedmogg@gmail.com>
2020-11-10 23:49:20 +0000

    took out an if statement

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,15 +22,13 @@ int create_file(const char *filename, char *text_content)
 		for (i = 0; text_content[i] != '\0'; i++)
 			;
 	}
-	if (filename != NULL)
-	{
-		fp = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-		if (fp == -1)
-			return (-1);
-	}
-		value = write(fp, text_content, i);
-		if (value == -1)
-			return (-1);
+	fp = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fp == -1)
+		return (-1);
+
+	value = write(fp, text_content, i);
+	if (value == -1)
+		return (-1);
 
 	close(fp);
 	return (1);
