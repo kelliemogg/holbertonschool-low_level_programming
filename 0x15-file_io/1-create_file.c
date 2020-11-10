3622aa0411d8ed6690a59c2da47f5c6e1b3dd5a4
Kellie Mogg <kelliedmogg@gmail.com>
2020-11-10 03:05:45 +0000

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,17 +14,17 @@ int create_file(const char *filename, char *text_content)
 {
 	int fp, value;
 
-	if (filename == NULL)
-		return (-1);
-
 	fp = open(filename, O_CREAT | O_TRUNC | O_RDWR, 600);
 	if (fp == -1)
 		return (-1);
 
-	value = write(STDOUT_FILENO, text_content, 1);
+	value = write(STDOUT_FILENO, text_content, 1024);
 	if (value == -1)
 		return (-1);
 
+	if (filename == NULL)
+                return (-1);
+
 	close(fp);
-	return (value);
+	return (1);
 }
