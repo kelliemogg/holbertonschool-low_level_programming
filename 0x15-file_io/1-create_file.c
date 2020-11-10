806278eaef580a46086399060c01f83566eb05dd
Kellie Mogg <kelliedmogg@gmail.com>
2020-11-10 00:49:07 +0000

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,23 +16,23 @@ int create_file(const char *filename, char *text_content)
 	ssize_t fd, count, value;
 
 	if (filename == NULL)
-		return (0);
+		return (-1);
 
-	ptr = malloc(sizeof(char) * letters);
+	ptr = malloc(sizeof(char) * (*text_content));
 	if (ptr == NULL)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-		return (0);
+		return (-1);
 
-	count = read(fd, ptr, letters);
+	count = read(fd, ptr, text_content);
 	if (count == -1)
-		return (0);
+		return (-1);
 
 	value = write(STDOUT_FILENO, ptr, count);
 	if (value == -1)
-		return (0);
+		return (-1);
 
 	free(ptr);
 	close(fd);
