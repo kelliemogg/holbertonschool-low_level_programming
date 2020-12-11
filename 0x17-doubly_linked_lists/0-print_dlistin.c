81d9da561e0c9131d8f41f1d4846136a5015bf95
Kellie Mogg <kelliedmogg@gmail.com>
2020-12-11 19:29:03 +0000

    first commit

---
+++ 0x17-doubly_linked_lists/0-print_dlistin.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - print all the elements
+ * description: from a list
+ * @h:
+ * Return: num of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+}
