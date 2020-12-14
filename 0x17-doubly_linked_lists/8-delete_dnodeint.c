c028d0344085d4c5e27faafc3205783235104fcb
Kellie Mogg <kelliedmogg@gmail.com>
2020-12-14 20:56:39 +0000

    first commit

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - delete it
+ * description: dlt at a node
+ * @head: head of list
+ * @index: index in list
+ * Return: 1 or -1
+ */
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	return (0);
+}
