3f2df7a222f1c58a6c0a92e7df6f06eab2692755
Kellie Mogg <kelliedmogg@gmail.com>
2021-01-16 03:33:01 +0000

    base file

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - del table
+ * description: delete a hash table
+ * @ht: hash table
+ * Return: void
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	if (ht == NULL)
+		return;
+}
