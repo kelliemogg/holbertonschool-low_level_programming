be4765ba020780b88dc503ff4ba196f19a9af6f0
Kellie Mogg <kelliedmogg@gmail.com>
2021-01-16 23:37:42 +0000

    duplicate value

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -17,6 +17,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (!ht || !key || !value || strcmp(key, "") == 0)
 		return (0);
 	new = malloc(sizeof(hash_node_t));
+	value = strdup(value);
 	new->key = (char *)key;
 	new->value = (char *)value;
 	new->next = NULL;
