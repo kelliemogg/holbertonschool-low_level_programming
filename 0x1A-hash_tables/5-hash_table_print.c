836b592f66ff5b71225972e24071c948d4bae255
Kellie Mogg <kelliedmogg@gmail.com>
2021-01-16 03:30:22 +0000

    base file

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a ht
+ * description: prints a hash table
+ * @ht: hash table
+ * Return: void
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	if (ht == NULL)
+		return;
+}
