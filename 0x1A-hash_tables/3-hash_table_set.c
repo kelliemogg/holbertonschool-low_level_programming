9e34aeca76f6abc8faeab02ccc6a68c4aecfa2ac
Kellie Mogg <kelliedmogg@gmail.com>
2021-01-18 20:45:43 +0000

    strdup key AND value

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -20,9 +20,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (!new)
 		return (0);
 
-	value = strdup(value);
-	new->key = (char *)key;
-	new->value = (char *)value;
+	value_copy = strdup(value);
+	key_copy = strdup(key);
+	new->key = (char *)key_copy;
+	new->value = (char *)value_copy;
 	new->next = NULL;
 
 	idx = key_index((const unsigned char *)(key), ht->size);
