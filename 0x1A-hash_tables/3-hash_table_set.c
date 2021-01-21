acb736e45756ecd0d18e6ee069914aaa75a2c577
Kellie Mogg <kelliedmogg@gmail.com>
2021-01-21 16:27:12 +0000

    3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,7 +14,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int idx = 0;
 	hash_node_t *new = NULL, *tmp = NULL;
 
-	if (!ht || !key || !value || strcmp(key, "") == 0)
+	if (!key || !value || strcmp(key, "") == 0)
 		return (0);
 	new = malloc(sizeof(hash_node_t));
 	if (!new)
@@ -28,10 +28,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	idx = key_index((const unsigned char *)(key), ht->size);
 
-	if (ht->array[idx] == NULL)
+	if (ht->array[idx] == NULL || strcmp(key, key))
 		ht->array[idx] = new;
 	else
 	{
+/* need for loop to look through linked list to check for matching keys */
 		tmp = ht->array[idx];
 		new->next = tmp;
 		ht->array[idx] = new;
--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -10,7 +10,7 @@
 void hash_table_print(const hash_table_t *ht)
 {
 	hash_node_t *node = NULL;
-	unsigned long int i = 0;
+	unsigned long int i = 0, on = 0;
 
 	if (ht == NULL)
 		return;
@@ -18,12 +18,13 @@ void hash_table_print(const hash_table_t *ht)
 	for (i = 0; i < ht->size; i++)
 	{
 		node = ht->array[i];
-		while (node)
+		while (on)
 		{
-			if (node->next != NULL)
+			if (node && node->next != NULL)
 			{
 				printf("\'%s\': \'%s\',", node->key, node->value);
 				node = node->next;
+				on = 1;
 			}
 			else
 				printf("%s: %s", node->key, node->value);
