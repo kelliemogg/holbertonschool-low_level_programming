f7191c19072add3cdacc2d9ba050a7f6f722c219
Kellie Mogg <kelliedmogg@gmail.com>
2021-04-22 03:58:56 +0000

    .

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -1,2 +1,28 @@
 #include "search_algos.h"
 
+/**
+* interpolation_search - inter search
+* description: search
+* @array: ptr to array
+* @size: size of array
+* @value: value searched for
+* Return: index or -1
+*/
+
+int interpolation_search(int *array, size_t size, int value)
+{
+	int i = 0;
+	int ctr = 0;
+
+	if (!array)
+		return (-1);
+	while (i >= 0 && (size_t)i < size)
+	{
+		printf("Value checked array[%i] = [%i]\n", ctr, array[i]);
+		if (array[i] == value)
+			return (ctr);
+		ctr++;
+		i++;
+	}
+	return (-1);
+}
